83363ae0f4430682c48239b5f41b871202361f19
payalts <payal.desai@retailogy.com>
2022-02-13 15:33:54 +0530

    changes

--- database/seeders/UserSeed.php
+++ database/seeders/UserSeed.php
@@ -15,8 +15,7 @@ class UserSeed extends Seeder
     {
         $items = [
             ['id' => 1,'name' => 'Admin', 'email' => 'admin@admin.com', 'password' => '123456','role_id' => 1], //admin
-            ['id' => 2,'name' => 'Payal Desai', 'email' => 'payal.desai06@gmail.com', 'password' => '123456','role_id' => 2], //user (loan seeker)
-            ['id' => 3,'name' => 'Loan Seeker', 'email' => 'loanseeker@mailinator.com', 'password' => '123456','role_id' => 2] //user (loan seeker)
+            ['id' => 2,'name' => 'Loan Seeker', 'email' => 'loanseeker@mailinator.com', 'password' => '123456','role_id' => 2] //user (loan seeker)
         ];
 
         foreach ($items as $item) {
